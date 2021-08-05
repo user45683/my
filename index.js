9f5ceef1651c7bf499da1bf3c49e810ad51021d6
Ваше Имя <you@example.com>
2021-08-06 00:52:30 +0300

    add

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html');});
 
-http.listen(port, () => { console.log('server run at :${port}');
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
