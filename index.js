90208dce1faab2e2461ad960b33bc48b7c09f40f
Ваше Имя <you@example.com>
2021-08-05 18:05:26 +0300

    add

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+const app = require('express')();
+const http = require('http').Server(app);
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html');});
+
+http.listen(port, () => { console.log('server run at http://localhost:${port}/');
+});
