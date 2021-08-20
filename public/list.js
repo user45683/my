720b91e6d6bb90e93129f19c3f5b6a45b744bca9
user45683 <gm2308483@gmail.com>
2021-08-20 21:31:17 +0300

    add list 00

--- public/list.js
+++ public/list.js
@@ -1,5 +1,9 @@
 
+let table = document.getElementById('myTable');
+let tr = document.createElement('tr');
 
-var listA = {
+let listA = [
+  code: 0000001 , name: 'Bella', pos: 'b7-04'},
+];
 
-};
+document.table.append(listA);
