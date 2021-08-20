e3d5fed48179dd13c133d80c3ee277b192d733b5
user45683 <gm2308483@gmail.com>
2021-08-20 21:02:27 +0300

    add 002

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const http = require('http').Server(app);
 const port = process.env.PORT || 3000;
 
 //app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html');});
-app.use(express.static(__dirname + 'public/'));
+app.use(express.static(__dirname + '/public'));
 
 http.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
