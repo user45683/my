8c24c2fade90b09fe139beeb71dd35c553e1f1cd
user45683 <gm2308483@gmail.com>
2021-08-21 01:43:02 +0300

    good

--- public/list.js
+++ public/list.js
@@ -1,13 +1,37 @@
 function listFunction(){
-  let table = document.getElementById('myTable');
-  let tr = document.createElement('tr');
-  let td = document.createElement('td');
+  let table = document.querySelector('#myTable');
+
+
+
   let arrList = [
-    {code: '000', name: 'l', pos: 'c56'},
+    {code: '000', name: 'l', pos: 'B7-20'},
+    {code: '000', name: 'l', pos: 'B7-19'},
+    {code: '000', name: 'l', pos: 'B7-18'},
+    {code: '000', name: 'Classico', pos: 'B7-17'},
+    {code: '000', name: 'Lazurro', pos: 'B7-17'},
+
     {code: '0000003', name: 'Bella', pos: 'b7-04'},
-    {code: '000', name: 'l', pos: 'c56'},
+    {code: '000', name: '2', pos: 'c56'},
+    {code: '000', name: '2', pos: 'c56'},
+    {code: '000', name: '2', pos: 'c56'},
+    {code: '000', name: '2', pos: 'c56'},
   ];
-    td.innerText = arrList[1].code;
-    table.appendChild(tr).appendChild(td);
+
+
+
+  for(let i = 0; i < arrList.length; i++){
+    let td0 = document.createElement('td');
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    let tr = document.createElement('tr');
+    table.appendChild(tr);
+    td0.innerText = arrList[i].code;
+    td1.innerText = arrList[i].name;
+    td2.innerText = arrList[i].pos;
+    tr.appendChild(td0);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+  }
 }
+
 listFunction();
