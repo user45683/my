d8acdfa86a68159c9a59faa544e3d95ef81a0416
user45683 <gm2308483@gmail.com>
2021-08-21 19:03:25 +0300

    barc

--- public/list.js
+++ public/list.js
@@ -6,7 +6,7 @@ function listFunction(){
   let arrList = [
         //tile
             //b7
-    {code: '', name: 'Classico', pos: 'B7-17'},
+    {code: '', name: 'Classico', pos: 'B7-17', barcode: '000000000'},
     {code: '', name: 'Lazurro', pos: 'B7-17'},
     {code: '', name: 'Bella', pos: 'b7-04'},
             //B6
@@ -71,14 +71,17 @@ function listFunction(){
     let td0 = document.createElement('td');
     let td1 = document.createElement('td');
     let td2 = document.createElement('td');
+    let td3 = document.createElement('td');
     let tr = document.createElement('tr');
     table.appendChild(tr);
     td0.innerText = arrList[i].code;
     td1.innerText = arrList[i].name;
     td2.innerText = arrList[i].pos;
+    td3.innerText = arrList[i].barcode;
     tr.appendChild(td0);
     tr.appendChild(td1);
     tr.appendChild(td2);
+    tr.appendChild(td3);
   }
 }
 
