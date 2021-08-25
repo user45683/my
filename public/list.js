f80cf5b91aaf4a85f876038db3dbce0f62b817c7
Your Name <you@example.com>
2021-08-25 05:03:33 +0300

    add

--- public/list.js
+++ public/list.js
@@ -1,6 +1,6 @@
 function listFunction(){
   let table = document.querySelector('#myTable');
-
+  let c = document.querySelector('.c');
 
   let arrList = [
 	  //инструменты
@@ -9,7 +9,7 @@ function listFunction(){
             //b7
             //B6
 
-{code: '396227', name: 'Панно Фристайл 5 20x20', pos: 'b6-08'},
+{code: '396227', name: 'Панно Фристайл 5 20x20', pos: 'b6-08', barcode: '000000000', img: 'https://images.unsplash.com/photo-1573804633927-bfcbcd909acd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z29vZ2xlJTIwbG9nb3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80'},
 {code: '712527', name: 'Плитка настенная Madera GT 60x25 Тип 2 Бежевый', pos: 'B6-06'},
 {code: '712530', name: 'Плитка настенная Montblanc GT 60x25 Тип 1 Серый', pos: 'B6-06'},
 {code: '712533', name: 'Бордюр Montblanc GT 60x6,5 Серый', pos: 'B6-06'},
@@ -23,52 +23,44 @@ function listFunction(){
         //стройматериалы
 {code: '493141', name: 'Затирка водоотталкивающая Ceresit CE 40, цвет антрацит, 2кг', pos: 'B5-13'},
 {code: '141304', name: 'Затирка водоотталкивающая Ceresit CE 40, цвет белый, 2кг', pos: 'B5-13'},
-    {code: '527695', name: 'Клей для плитки Ceresit CM 14, 25кг', pos: 'B5-11'},
+{code: '527695', name: 'Клей для плитки Ceresit CM 14, 25кг', pos: 'B5-11'},
         //door
-    {code: '671588', name: 'Полотно дверное глухое 80x200, ламинация, цвет венге', pos: 'B3-20'},
-    {code: '764892', name: 'Наличник плоский, 3D покрытие 2150x70x8мм, светло-серый', pos: ''},
-    {code: '764893', name: 'Коробка прямая, 3D покрытие 2070x74x28мм, светло-серый', pos: ''},
-    {code: '764883', name: 'Полотно дверное остекленное Техно 642 2000x700мм, 3D покрытие, цвет светло-серый', pos: ''},
+{code: '671588', name: 'Полотно дверное глухое 80x200, ламинация, цвет венге', pos: 'B3-20'},
+{code: '764892', name: 'Наличник плоский, 3D покрытие 2150x70x8мм, светло-серый', pos: ''},
+{code: '764893', name: 'Коробка прямая, 3D покрытие 2070x74x28мм, светло-серый', pos: ''},
+{code: '764883', name: 'Полотно дверное остекленное Техно 642 2000x700мм, 3D покрытие, цвет светло-серый', pos: ''},
         //steel door
-    {code: '709442', name: 'Дверь входная металлическая Рубикон Царга 2050x960 Левая, Лиственница белая', pos: ''},
-    {code: '728473', name: 'Дверь входная Стройгост 5 РФ 2050x860 Левая, Рустикальный дуб', pos: ''},
-    {code: '733560', name: 'Дверь входная Dominanta 2050x860 Правая, Рустикальный дуб', pos: ''},
+{code: '709442', name: 'Дверь входная металлическая Рубикон Царга 2050x960 Левая, Лиственница белая', pos: ''},
+{code: '728473', name: 'Дверь входная Стройгост 5 РФ 2050x860 Левая, Рустикальный дуб', pos: ''},
+{code: '733560', name: 'Дверь входная Dominanta 2050x860 Правая, Рустикальный дуб', pos: ''},
         //season products
-    {code: '096066', name: 'Пленка п/э ширина 6,0м 150мкм', pos: ''},
-    {code: '161519', name: 'Канистра-бочка 40л с навесными ручками M680', pos: 'С14-10'},
+{code: '096066', name: 'Пленка п/э ширина 6,0м 150мкм', pos: ''},
+{code: '161519', name: 'Канистра-бочка 40л с навесными ручками M680', pos: 'С14-10'},
         //paints
-    {code: '489884', name: 'Пена монтажная KRASS 890мл V69 ПРОФИ', pos: ''},
-
-    {code: '036129', name: 'Грунтовка ГФ-021 2,5кг красно-коричневая Л-С', pos: ''},
-    {code: '031078', name: 'Грунтовка ГФ-21 20кг красно-коричневая Л-С', pos: ''},
-
-    {code: '674992', name: 'Состав FAKTURA антисептический невымываемый ХМФ-БФ, 10л', pos: ''},
+{code: '489884', name: 'Пена монтажная KRASS 890мл V69 ПРОФИ', pos: ''},
+{code: '036129', name: 'Грунтовка ГФ-021 2,5кг красно-коричневая Л-С', pos: ''},
+{code: '031078', name: 'Грунтовка ГФ-21 20кг красно-коричневая Л-С', pos: ''},
+{code: '674992', name: 'Состав FAKTURA антисептический невымываемый ХМФ-БФ, 10л', pos: ''},
 
         //plumbing
-    {code: '762308', name: 'Зеркальный шкаф Итана Анкона 50 500x150x790 (Белый глянец)', pos: ''},
-    {code: '629816', name: 'Экран для ванны МетаКам 1,68м белый', pos: 'A15-17'},
+{code: '762308', name: 'Зеркальный шкаф Итана Анкона 50 500x150x790 (Белый глянец)', pos: ''},
+{code: '629816', name: 'Экран для ванны МетаКам 1,68м белый', pos: 'A15-17'},
 
 	  //напольные покрытия
 	  	//плинтус
-    {code: '295331', name: 'Плинтус ПВХ 2500x66x2 мм. Дуб Белый - 622 ESQUERO', pos: ''},
+{code: '295331', name: 'Плинтус ПВХ 2500x66x2 мм. Дуб Белый - 622 ESQUERO', pos: ''},
 	 	 //ламинат
-    {code: '759180', name: '', pos: 'Л4-10'},
-    {code: '787489', name: '', pos: 'L3-4'},
-
-
-
+{code: '759180', name: '', pos: 'Л4-10'},
+{code: '787489', name: '', pos: 'L3-4'},
         //kitchen
-    {code: '755222', name: 'Фасад Аркадия 357x596 ясень анкор светлый', pos: ''},
-
-    {code: '755226', name: 'Фасад Аркадия 716x397 милк', pos: ''},
-    {code: '755222', name: 'Фасад Аркадия 357x596 милк', pos: ''},
-
-    {code: '676711', name: 'Корпус 72*40 навесной 400x720', pos: ''},
-    {code: '676712', name: 'Корпус 72*60 навесной 600x720', pos: ''},
-
-    {code: '676702', name: 'Корпус стола 1С 1Я 400 (Белый)', pos: ''},
+{code: '755222', name: 'Фасад Аркадия 357x596 ясень анкор светлый', pos: ''},
+{code: '755226', name: 'Фасад Аркадия 716x397 милк', pos: ''},
+{code: '755222', name: 'Фасад Аркадия 357x596 милк', pos: ''},
+{code: '676711', name: 'Корпус 72*40 навесной 400x720', pos: ''},
+{code: '676712', name: 'Корпус 72*60 навесной 600x720', pos: ''},
+{code: '676702', name: 'Корпус стола 1С 1Я 400 (Белый)', pos: ''},
 	  //мебель для домашнего офиса
-    {code: '715699', name: 'Стул складной JACK BLACK (CH) V-CERATA', pos: 'AP-3'},
+{code: '715699', name: 'Стул складной JACK BLACK (CH) V-CERATA', pos: 'AP-3'},
   ];
 
 
@@ -86,6 +78,25 @@ function listFunction(){
     tr.appendChild(td0);
     tr.appendChild(td1);
     tr.appendChild(td2);
+  }
+
+  for(let i = 0; i < arrList.length; i++){
+    let card = document.createElement('div');
+    card.classList.add('card');	  
+    let h3Code = document.createElement('h3');
+    let pName = document.createElement('p');
+    let imgImage = document.createElement('img');
+    let pBarcode = document.createElement('p');
+    h3Code.innerText = 'код: '+arrList[i].code;
+    pName.innerText = arrList[i].name;
+    imgImage.src = arrList[i].img;
+    pBarcode.innerText = 'ШК: '+arrList[i].barcode;
+
+    c.appendChild(card);
+    card.appendChild(imgImage);
+    card.appendChild(h3Code);
+    card.appendChild(pName);
+    card.appendChild(pBarcode);
   }
 
 }
