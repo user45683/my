c36410caf47e060eab664e2bae15d40f9838bcc2
Your Name <you@example.com>
2021-08-27 18:41:35 +0300

    a

--- public/list.js
+++ public/list.js
@@ -9,8 +9,8 @@ function listFunction(){
             //b7
             //B6
 
-{code: '396227', name: 'Панно Фристайл 5 20x20', pos: 'b6-08', barcode: '000000000', img: 'https://images.unsplash.com/photo-1573804633927-bfcbcd909acd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z29vZ2xlJTIwbG9nb3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80'},
-{code: '712527', name: 'Плитка настенная Madera GT 60x25 Тип 2 Бежевый', pos: 'B6-06'},
+{code: '396227', name: 'Панно Фристайл 5 20x20', pos: 'b6-08', price: '000', barcode: '000000000', img: 'https://images.unsplash.com/photo-1573804633927-bfcbcd909acd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z29vZ2xlJTIwbG9nb3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80'},
+{code: '712527', name: 'Плитка настенная Madera GT 60x25 Тип 2 Бежевый', pos: 'B6-06', price: '000', barcode: '000000000', img: 'https://images.unsplash.com/photo-1573804633927-bfcbcd909acd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z29vZ2xlJTIwbG9nb3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80'},
 {code: '712530', name: 'Плитка настенная Montblanc GT 60x25 Тип 1 Серый', pos: 'B6-06'},
 {code: '712533', name: 'Бордюр Montblanc GT 60x6,5 Серый', pos: 'B6-06'},
             //B5
@@ -19,6 +19,9 @@ function listFunction(){
 {code: '732486', name: 'Плитка настенная Мартиника 30*60 верх', pos: ''},
 {code: '732488', name: 'Плитка напольная Мартиника 40*40', pos: ''},
 {code: '670132', name: 'Плитка напольная Sklad GT 40*40 3SK0022', pos: ''},
+
+
+{code: '749213' ,name: 'Керамогранит 40x40 LAZURRO Бежевый 3L1870 (1.12)', pos: '', price: '', img: '', barcode: ''},
 
         //стройматериалы
 {code: '493141', name: 'Затирка водоотталкивающая Ceresit CE 40, цвет антрацит, 2кг', pos: 'B5-13'},
@@ -29,10 +32,13 @@ function listFunction(){
 {code: '764892', name: 'Наличник плоский, 3D покрытие 2150x70x8мм, светло-серый', pos: ''},
 {code: '764893', name: 'Коробка прямая, 3D покрытие 2070x74x28мм, светло-серый', pos: ''},
 {code: '764883', name: 'Полотно дверное остекленное Техно 642 2000x700мм, 3D покрытие, цвет светло-серый', pos: ''},
-        //steel door
+        //двери, фурнитура дверная
 {code: '709442', name: 'Дверь входная металлическая Рубикон Царга 2050x960 Левая, Лиственница белая', pos: ''},
 {code: '728473', name: 'Дверь входная Стройгост 5 РФ 2050x860 Левая, Рустикальный дуб', pos: ''},
 {code: '733560', name: 'Дверь входная Dominanta 2050x860 Правая, Рустикальный дуб', pos: ''},
+		//фурнитура
+{code: '383836' ,name: 'Петля дверная универсальная Palladium N 500 C-S-4" MG(магма)', pos: '', price: '490', img: '', barcode: ''},
+
         //season products
 {code: '096066', name: 'Пленка п/э ширина 6,0м 150мкм', pos: ''},
 {code: '161519', name: 'Канистра-бочка 40л с навесными ручками M680', pos: 'С14-10'},
@@ -61,6 +67,10 @@ function listFunction(){
 {code: '676702', name: 'Корпус стола 1С 1Я 400 (Белый)', pos: ''},
 	  //мебель для домашнего офиса
 {code: '715699', name: 'Стул складной JACK BLACK (CH) V-CERATA', pos: 'AP-3'},
+	//фурнитура мебельная
+{code: '676762' ,name: 'Ручка скоба ', pos: '', price: '', img: '', barcode: ''},
+{code: '' ,name: '', pos: '', price: '', img: '', barcode: ''},
+{code: '' ,name: '', pos: '', price: '', img: '', barcode: ''},
   ];
 
 
@@ -87,15 +97,18 @@ function listFunction(){
     let pName = document.createElement('p');
     let imgImage = document.createElement('img');
     let pBarcode = document.createElement('p');
+    let pPrice = document.createElement('p');
     h3Code.innerText = arrList[i].code;
     pName.innerText = arrList[i].name;
     imgImage.src = arrList[i].img;
+    pPrice.innerText = 'цена: '+arrList[i].price;
     pBarcode.innerText = 'шк: '+arrList[i].barcode;
 
     c.appendChild(card);
     card.appendChild(imgImage);
     card.appendChild(h3Code);
     card.appendChild(pName);
+    card.appendChild(pPrice);
     card.appendChild(pBarcode);
   }
 
